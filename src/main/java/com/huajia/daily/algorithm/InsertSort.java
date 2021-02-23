ac8fc63dc619ce5b00b0ede458543f6791e4277c
huajia <jiangjianping@souche.com>
2021-02-23 22:41:19 +0800

    add: 插入排序。

---
+++ src/main/java/com/huajia/daily/algorithm/InsertSort.java
@@ -0,0 +1,24 @@
+package com.huajia.daily.algorithm;
+
+import java.util.Arrays;
+
+public class InsertSort {
+    public static void main(String[] args) {
+        int[] arr = {1,7,2,5,1};
+        System.out.println(Arrays.toString(sort(arr)));
+    }
+
+    private static int[] sort(int[] arr) {
+        for(int i=1; i<arr.length; i++) {
+            int index = i;
+            int temp = arr[i];
+            while(index > 0 && temp <= arr[index - 1]) {
+                arr[index] = arr[index - 1];
+                index--;
+            }
+
+            arr[index] = temp;
+        }
+        return arr;
+    }
+}
